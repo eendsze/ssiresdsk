8e090ebbd5123af6baa804a2d70003183b3bda98
Endre Szesztay <endre.szesztay@gmail.com>
2021-10-09 12:12:40 +0100

    vezerlo

--- modelvezerlo.py
+++ modelvezerlo.py
@@ -3,21 +3,45 @@ import math
 import pygame as pg
 import joystick
 import szabalyzoelemek
+import megjelenit
+import serial, json
 from modellek import *
 
-fps = 50
+fps = 100
 dt = 1.0/fps
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 
 def main():
+    try:
+        ser = serial.Serial("/dev/ttyACM0", 115200)
+    except Exception as e:
+        print(str(e))
+        exit()
+    ser.timeout = 0
+
+    if ser.isOpen():
+        ser.close()
+
+    try:
+        ser.open()
+    except Exception as e:
+        print(str(e))
+        exit()
+    ser.flushInput()
+
     pg.init()
     becsultDict = Hajomodell2Becs
     clock = pg.time.Clock()
  #   screen = pg.display.set_mode((1000, 1000), pg.RESIZABLE)
     joy = joystick.myJoystic()
+    megj = megjelenit.disp()
     modell = szabalyzoelemek.modell(becsultDict)
     PID = szabalyzoelemek.PIDcontroller(becsultDict)
     Akt = [ 0, 0, 0, 0] # orrsugar, farsugar, jobb motor, bal motor
+    #Vins [self.vx, self.vy, V[2]]
+    Vins = [0.0] * 3
+    # AKTUATOR 100%-HOZ TARTOZO FESZULTSEGEK orrsugar, farsugar, jobb motor, bal motor
+    Voltages = [6.0, 6.0, 3.0, 3.0]
     AktFormed = Akt
 
     while 1:
@@ -39,11 +63,23 @@ def main():
         Vmod = modell.process(dt, Akt, Vins)
         # A PID megkapja a modell altal josolt sebesseget es az input vektort is, ezekbol szamolja az aktuatorok jeleit
         Akt = PID.process(dt, Vmod, J)
-        AktFormed = map(lambda x: szabalyzoelemek.actForm(x), Akt)
+        AktFormed = list(map(lambda x, v: szabalyzoelemek.actForm(x) * v, Akt, Voltages))
 
-# ezt el is kell kuldeni a motoroknak
+        # ezt el is kell kuldeni a motoroknak
+        command = f"start {int(AktFormed[0]*1000)} {int(AktFormed[1]*1000)}  {int(AktFormed[2]*1000)}  {int(AktFormed[3]*1000)}  end \n"
+        ser.write(command.encode())
+        # be isolvasom az aramot, ha van mit
+        try:
+            res = ser.readline()
+            pwmDict = json.loads(res)
+            curr =[pwmDict['I1'], pwmDict['I2'], pwmDict['I3'], pwmDict['I4']]
+            megj.setActCurr(curr)
+        except:
+            pass
 
         # Ezek a megjelenites dolgai, a szabalyzasba nem szol bele
+        megj.setActVolt(AktFormed)
+        megj.show()
         # Ez is megjelenites
 #        pg.display.update()
         clock.tick(fps)
