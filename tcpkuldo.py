2a8bac5335ad964e657642b20607dd30918c4542
eendsze <endre.szesztay@gmail.com>
2021-10-11 08:56:09 +0200

    x

--- tcpkuldo.py
+++ tcpkuldo.py
@@ -5,12 +5,12 @@ import netifaces
 
 bport = 6544
 bss = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#bss.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+bss.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 gwadd = netifaces.gateways()[2][0][0]
 b = socket.inet_aton(gwadd)
 badd = f'{b[0]}.{b[1]}.{b[2]}.255'
 print('Broadcast address:' + badd)
-bss.bind(('0.0.0.0', bport))
+#bss.bind(('0.0.0.0', bport))
 
 while True:
     bss.sendto(b'sanyi\n', (badd, bport))
